d05d6c5e7c98e5ad48a6d91fa3eb57459bd607b1
ahinkim <ahin98@naver.com>
2023-05-22 19:56:44 +0900

    [#2] refactor: 인증 메일 기능 개선 이메일 글자 수 제한 기능 추가 구현

--- src/main/java/com/moa/member/dto/EmailRequestDto.java
+++ src/main/java/com/moa/member/dto/EmailRequestDto.java
@@ -2,6 +2,7 @@ package com.moa.member.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class EmailRequestDto {
 	@Email
-	@NotBlank(message = "이메일(필수)")
+	@NotBlank(message = "이메일은 필수 입력 항목입니다.")
+	@Size(max = 100, message = "이메일은 최대 100자까지 입력 가능합니다.")
 	private String email;
-
 }
