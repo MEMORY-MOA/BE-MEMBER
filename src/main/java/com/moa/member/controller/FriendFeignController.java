f2a2606e3839e88f4dbc88596108d950f969a856
yejin013 <yejin013@naver.com>
2023-07-22 19:13:30 +0900

    [#43] doc: api 문서 추가

--- src/main/java/com/moa/member/controller/FriendFeignController.java
+++ src/main/java/com/moa/member/controller/FriendFeignController.java
@@ -3,6 +3,7 @@ package com.moa.member.controller;
 import com.moa.member.controller.request.FriendFeignRequest;
 import com.moa.member.service.FriendFeignService;
 import com.moa.member.mapstruct.FriendMapper;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,7 @@ public class FriendFeignController {
 
 	@PostMapping("/acceptance")
 	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "친구초대수락 API_yejin", description = "타임캡슐 내 친구 초대 후 사용되는 api로, 프론트는 사용X")
 	public void friendAccept(@RequestBody FriendFeignRequest friendFeignRequest) {
 		friendFeignService.friendAccept(FriendMapper.instance.feignRequestToDto(friendFeignRequest));
 	}
