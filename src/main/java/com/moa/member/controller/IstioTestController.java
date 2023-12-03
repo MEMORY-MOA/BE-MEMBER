def2e40b1bbef77c2ecf3956ab5e48a6d63f6716
ahinkim <ahin98@naver.com>
2023-12-03 11:14:46 +0900

    feat: istio-test 기능 추가

---
+++ src/main/java/com/moa/member/controller/IstioTestController.java
@@ -0,0 +1,30 @@
+package com.moa.member.controller;
+
+import java.util.Random;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moa.member.controller.response.ResponseDto;
+
+@RestController
+@RequestMapping("/users")
+public class IstioTestController {
+
+	@GetMapping("/istio-test/ping-with-random-delay")
+	public ResponseEntity<ResponseDto<Object>> pingWithRandomDelay() throws InterruptedException {
+		int r = new Random().nextInt(2000, 4000);
+		Thread.sleep(r);
+
+		ResponseDto response = ResponseDto.builder()
+			.httpStatus(HttpStatus.OK)
+			.msg("Istio Test")
+			.data(null)
+			.build();
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+}
